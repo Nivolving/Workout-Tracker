35fa64ac546b2bac27423e103d2011456918fa5b
Nivolving <nivethapartheeban@gmail.com>
2020-07-27 21:05:56 -0700

    Models added

---
+++ Develop/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const db = require("./models");
+
+app.use(logger("dev"));
+
+//Add data processing middleware.
+app.use(express.urlencoded({ extended : true }));
+app.use(express.json());
+
+//Use the public directory to serve the static files
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+
+//Open listener to begin listening to requests
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
